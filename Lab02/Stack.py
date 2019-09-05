a6ceb034d0fa0224ece39d0ee56094bc2a11a245
9ine2543 <9.nine2543@gmail.com>
2019-09-05 17:06:20 +0700

    Lab_02

--- Lab02/Stack.py
+++
@@ -1,6 +0,0 @@
-class Stack:
-    def __init__(self, list = None):
-        if list == None:
-            self.items = []
-        else :
-            self.items = list
--- Lab02/lab02_1.py
+++ Lab02/lab02_1.py
@@ -1,24 +1,4 @@
-class Stack:
-    def __init__(self, list=None):
-        if list == None:
-            self.items = []
-        else:
-            self.items = list
-
-    def push(self, i):
-        self.items.append(i)
-
-    def pop(self):
-        return self.items.pop()
-
-    def size(self):
-        return len(self.items)
-
-    def isEmpty(self):
-        return len(self.items) == 0
-
-    def peek(self):
-        return self.items[len(self.items) - 1]
+from Class import Stack
 
 test = Stack()
 name = input("Enter your name : ")
