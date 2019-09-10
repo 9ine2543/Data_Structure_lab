3f5113fb0ec77312ea27b16f8e6f28d581a56919
NINE <9.nine2543@gmail.com>
2019-09-10 21:46:04 +0700

    Lab_04

--- Lab04/Scramble.py
+++ Lab04/Scramble.py
@@ -1,42 +1,47 @@
-from Class import List
-from Class import node
+from Class import List,node
 
 
 def bottomUp(l, num):
-    count = num / 100.0 * l.size
-    for i in range (0,int(count)):
-        l.append(l.removeHead().data)
-
+    count = num / 100.0 * l.getSize()
+    temp = l.head
+    while temp.next is not None:
+        temp = temp.next
+    temp.next = l.head
+    while (count != 0):
+        temp = temp.next
+        count -= 1
+    l.head = temp.next
+    temp.next = None
     return l
 
 def riffle(l,num):
-    count = num / 100.0 * l.size
-    count2 = l.size - count
-    temp = l.head
-    temp2 = l.head
-    for i in range(0,int(count)):
-        temp2 = temp2.next
-    if(count >= count2):
-        l.before(temp2.data).next = None
-        for i in range (0,int(count2)):
-            nexttemp = temp.next
-            nexttemp2 = temp2.next
-            temp.next = temp2
-            temp2.next = nexttemp
-            temp = nexttemp
-            temp2 = nexttemp2
+    # count = num / 100.0 * l.size
+    # count2 = l.size - count
+    # temp = l.head
+    # temp2 = l.head
+    # for i in range(0,int(count)):
+    #     temp2 = temp2.next
+    # if(count >= count2):
+    #     l.before(temp2.data).next = None
+    #     for i in range (0,int(count2)):
+    #         nexttemp = temp.next
+    #         nexttemp2 = temp2.next
+    #         temp.next = temp2
+    #         temp2.next = nexttemp
+    #         temp = nexttemp
+    #         temp2 = nexttemp2
 
-    elif(count < count2):
-        for i in range (0,int(count)):
-            if (i != int(count) - 1):
-                nexttemp = temp.next
-                nexttemp2 = temp2.next
-                temp.next = temp2
-                temp2.next = nexttemp
-                temp = nexttemp
-                temp2 = nexttemp2
-            elif (i == int(count)-1):
-                temp.next = temp2
+    # elif(count < count2):
+    #     for i in range (0,int(count)):
+    #         if (i != int(count) - 1):
+    #             nexttemp = temp.next
+    #             nexttemp2 = temp2.next
+    #             temp.next = temp2
+    #             temp2.next = nexttemp
+    #             temp = nexttemp
+    #             temp2 = nexttemp2
+    #         elif (i == int(count)-1):
+    #             temp.next = temp2
     return l
 
 # def deriffle(l,num):
