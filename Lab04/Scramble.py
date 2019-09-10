ced187ad3e419656e724eed7555de90964269ce2
9ine2543 <9.nine2543@gmail.com>
2019-09-10 13:42:51 +0700

    Lab_04

--- Lab04/Scramble.py
+++ Lab04/Scramble.py
@@ -17,12 +17,41 @@ def bottomUp(l, num):
 
 def riffle(l,num):
     count = num / 100.0 * l.size
+    count2 = l.size - count
+    temp = l.head
+    temp2 = l.head
+    for i in range(0,int(count)):
+        temp2 = temp2.next
+    if(count >= count2):
+        l.before(temp2.data).next = None
+        for i in range (0,int(count2)):
+            nexttemp = temp.next
+            nexttemp2 = temp2.next
+            temp.next = temp2
+            temp2.next = nexttemp
+            temp = nexttemp
+            temp2 = nexttemp2
+
+    elif(count < count2):
+        for i in range (0,int(count)):
+            if (i != int(count) - 1):
+                nexttemp = temp.next
+                nexttemp2 = temp2.next
+                temp.next = temp2
+                temp2.next = nexttemp
+                temp = nexttemp
+                temp2 = nexttemp2
+            elif (i == int(count)-1):
+                temp.next = temp2
+    return l
 
 l = List(node(1))
 for i in range(2, 11):
     l.append(i)
 print(l)
-percent = int(input("Enter your percent to bottom up:"))
-print(bottomUp(l,percent))
+percent = int(input("Enter your percent to bottom up : "))
+print(bottomUp(l,percent)) 
+rif = int (input("Enter your percent to riffle : "))
+print(riffle(l,rif))
 
 
