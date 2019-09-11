3d7f80c1c89ba61d47b5b7a31ba3c46316f97916
9ine2543 <9.nine2543@gmail.com>
2019-09-11 16:01:22 +0700

    Lab_05

--- Lab05/Function.py
+++ Lab05/Function.py
@@ -13,3 +13,25 @@ def fac(n):
         n = n * fac(n-1)
     return n
 print(fac(5))
+
+def sum1Ton(n):
+    if n is not 1:
+        n= n + sum1Ton(n-1)
+    return n
+print(sum1Ton(5))
+
+def printNto1(n):
+    if n is not 1:
+        print(n,end = ' ')
+        n = printNto1(n-1)
+    return n
+print(printNto1(5))
+
+def print1ToN(n):
+    if n is 1:
+        print(n,end = ' ')
+    else:
+        print1ToN(n-1)
+        print(n,end = ' ')
+    
+print1ToN(5)
