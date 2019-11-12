dc78fdbc6f9b9ed2d1b4a7a7a36889f28a39555f
9ine2543 <9.nine2543@gmail.com>
2019-11-12 15:28:11 +0700

    Lab08

--- Test_Exam/Linked_List.py
+++ Test_Exam/Linked_List.py
@@ -1,37 +1,65 @@
-class singly_list:
-    class node:
-        def __init__(self, data = None,next = None):
-                self.data = data
-                if next is None:
-                    self.next = None
-                    self.prev = None
-                else:
-                    self.next = next
-                    self.next.prev = self
-    
-    # def __init__(self, head = None): #V.1
-    #     if head is None:
-    #         self.head = None
-    #         self.size = 0
-    #     else:
-    #         self.head = head 
-    #         self.size = 1
-    def __init__(self):
-        self.head = self.tail = node() #dummy
-        self.size = 0
-    def addHead(self,data):
-        self.head.next = node(data,self.head.next)
-        if self.isEmpty():
-            self.tail = self.head.next
-        self.size += 1 
-    def getSize(self):
-        return self.size
-    def isEmpty(self):
-        return self.size == 0
-    def append(self,data):
-        if self.isEmpty :
-            self.head.next = self.tail = node(data,self.head.next)
-            self.size += 1
-        else:
-            self.tail.next = node(data)
-            self.size += 1
+class node:
+		def __init__(self, data = None, next = None):
+			self.data = data;
+			if next is None:
+				self.next = None
+				self.prev = None
+			else:
+				self.next = next
+				self.next.prev = self
+
+
+class LinkedList:
+
+	def __init__(self, head = None): #V.1
+		if head is None:
+			self.head = self.tail = node()
+			self.size = 0
+		else:
+			self.head = head 
+			self.size = 1
+	def __str__(self):
+		temp = self.head.next #frist node
+		s = ''
+		while temp.next is not None:
+			s = s + str(temp.data) + ' '
+			temp = temp.next
+		return s
+
+	def addHead(self,data):
+		self.head.next = node(data,self.head.next)
+		if self.isEmpty():
+			self.tail = self.head.next
+		self.size += 1 
+
+	def getSize(self):
+		return self.size
+
+	def isEmpty(self):
+		return self.size == 0
+
+	def append(self,data):
+		if self.isEmpty :
+			self.head.next = self.tail = node(data,self.head.next)
+			self.size += 1
+		else:
+			self.tail.next = node(data)
+			self.size += 1
+
+	def nodeAt(self,index):
+		temp = self.head.next
+		for i in range(index):
+			temp = temp.next
+		return temp
+
+test = LinkedList()
+test.addHead(0)
+print(test)
+test.append(1)
+print(test)
+test.append(2)
+print(test)
+test.append(3)
+print(test)
+test.append(4)
+print(test)
