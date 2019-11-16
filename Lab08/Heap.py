6b95d9a04a7e700e0b3a0856960acb5da5012994
9ine2543 <9.nine2543@gmail.com>
2019-11-16 14:33:57 +0700

    Lab08

---
+++ Lab08/Heap.py
@@ -0,0 +1,61 @@
+class node:
+	def __init__(self, data = None, next = None):
+		self.data = data;
+		if next is None:
+			self.next = None
+			self.prev = None
+		else:
+			self.next = next
+			self.next.prev = self
+	def setNext(self,next):
+		self.next = next
+		self.next.prev = self
+
+class LinkedList:
+	def __init__(self, head = None): #V.1
+		if head is None:
+			self.head = self.tail = node()
+			self.size = 0
+		else:
+			self.head = head 
+			self.size = 1
+
+	def __str__(self):
+		temp = self.head.next #frist node
+		s = ''
+		while temp.next is not None:
+			s = s + str(temp.data) + ' '
+			temp = temp.next
+		s = s + str(temp.data)
+		return s
+
+	def addHead(self,data):
+		self.head.next = node(data,self.head.next)
+		if self.isEmpty():
+			self.tail = self.head.next
+		self.size += 1 
+
+	def getSize(self):
+		return self.size
+
+	def isEmpty(self):
+		return self.size == 0
+
+	def append(self,data):
+		if self.isEmpty() :
+			self.head.next = self.tail = node(data,self.head.next)
+			self.size += 1
+		else:
+			self.tail.setNext(node(data))
+			self.tail = self.tail.next
+			self.size += 1
+
+	def nodeAt(self,index):
+		temp = self.head.next
+		for i in range(index):
+			temp = temp.next
+		return temp
+
+
+def insertMinHeap(h,i):
+	pass
